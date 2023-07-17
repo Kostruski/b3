70787ea3f6107d4e9682f6b7eeff4a245eab9132
Marek.Kostruski <marek.kostruski@bytamic.com>
2023-07-17 10:44:58 +0200

    add react

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from "./App";
 
-const root = createRoot(document.body);
+document.body.innerHTML = '<div id="app"></div>'
+
+const root = createRoot(document.getElementById('app'));
 
 root.render(<App/>)
